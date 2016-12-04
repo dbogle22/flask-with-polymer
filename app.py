58ea588ae2b7e83f41f129589aa0eab5d59e7901
dbogle <dbogle22@gmail.com>
2016-12-03 18:32:13 -0700

    Added a route that will catch all other routes. Fixes issue where refresh will cause 404

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return app.send_static_file('index.html')
+
+@app.route('/<path:the_path>')
+def all_other_routes(the_path):
+    return app.send_static_file('index.html')
